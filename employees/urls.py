d663cd48631d7ec90ad9f4d347c609da7864f5f1
Ankit Kumar <ankitkumar13122003@gmail.com>
2025-11-09 16:39:27 +0530

    Made employees tab functional
    - Add employees list view and template
    - Link sidebar Employees tab to employees list page
    - Update related navigation links and redirects
    - Restrict access to HR/Admin users only

--- employees/urls.py
+++ employees/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from employees import views
 
 urlpatterns = [
+    path('', views.list_employees, name='list_employees'),
     path('add/', views.add_employee, name='add_employee'),
 ]
 
